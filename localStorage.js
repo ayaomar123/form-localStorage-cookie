bb99bd0fe0ab30e8da92feec3113bbac3873f9e1
ayaomar123 <44114766+ayaomar123@users.noreply.github.com>
2020-07-05 15:28:56 +0300

    second-commit

--- localStorage.js
+++ localStorage.js
@@ -1,26 +1,37 @@
- // create an array
-        var myArr = [];
+var myArr=[];
+var myObj={
+    name: "",
+    job:"",
+    Phone:""
+    }
         
-        function pushData()
-        {
-            var inputText = document.getElementById('inputText').value;
-            var job = document.getElementById('job').value;
-            var Phone = document.getElementById('Phone').value;
-            myArr.push(inputText);
-            myArr.push(job);
-            myArr.push(Phone);
+    function pushData(){
+        var name = document.getElementById('name').value;
+        var job = document.getElementById('job').value;
+        var Phone = document.getElementById('Phone').value;
+        myObj.name=name;
+        myObj.job=job;
+        myObj.Phone=Phone;
+        myArr.push(myObj);
+
+        var pval = "";
             
-            var pval = "";
-            
-            for(i = 0; i < myArr.length; i++)
-            {
-                pval = pval + myArr[i] + "<br/>";
+        for(i = 0; i < myArr.length; i++){
+            pval = pval + myArr[i] + "<br/>";
             }
-            document.getElementById('pText').innerHTML = pval;
             console.log(myArr);
-                }  
+        }  
         const submitbtn = document.getElementById('submit');
         submitbtn.addEventListener('click',pushData);
+       
         submitbtn.addEventListener('click',()=>{
-        localStorage.setItem('user',JSON.stringify(myArr));
+        if(localStorage.getItem(Phone.value)){
+            alert('this item exist');
+        }
+        else{
+            localStorage.setItem(Phone.value,JSON.stringify(myObj) );
+            };
         });
+
+       
+        
